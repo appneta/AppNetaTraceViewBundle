75b32e2d92c730f75e5d38fde87c706b6af5bbf0
James Meickle <jmeickle@appneta.com>
2013-09-20 17:27:49 -0400

    Replace the TraceView bundle with a service provider containing a compiler pass.

---
+++ lib/Drupal/traceview/DependencyInjection/Compiler/RegisterTraceViewTwigPass.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\traceview\DependencyInjection\Compiler\RegisterTraceViewTwigPass.
+ */
+
+namespace Drupal\traceview\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+
+/**
+ * Register a TraceView-modified version of Twig.
+ */
+class RegisterTraceViewTwigPass implements CompilerPassInterface {
+
+  /**
+   * Adds services tagged 'twig.extension' to the Twig service container.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerBuilder $container
+   *   The container to process.
+   */
+  public function process(ContainerBuilder $container) {
+    if (!$container->hasDefinition('twig')) {
+      return;
+    }
+
+    $twig = $container->getDefinition('twig');
+
+    // Modify the second argument, which is an array containing configuration.
+    $twig_config = $twig->getArgument(1);
+    $twig_config['base_template_class'] = 'Drupal\traceview\Template\TraceViewTwigTemplate';
+    $twig->replaceArgument(1, $twig_config);
+    return;
+  }
+}
