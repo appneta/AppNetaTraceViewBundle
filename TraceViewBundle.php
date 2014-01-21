11f38e97225468def9dab9eb0011083f195e4077
James Meickle <eronarn@gmail.com>
2014-01-21 19:03:28 +0000

    Move the Twig compiler pass into a bundle.

--- TraceViewBundle.php
+++ TraceViewBundle.php
@@ -5,11 +5,13 @@
  * Contains TraceViewBundle.
  */
 
-namespace AppNeta\TraceView\TraceViewBundle;
+namespace AppNeta\TraceViewBundle;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+use AppNeta\TraceViewBundle\DependencyInjection\Compiler\RegisterTraceViewTwigPass;
 
 /**
  * TraceView dependency injection container.
@@ -20,9 +22,9 @@ class TraceViewBundle extends Bundle {
    * Overrides Symfony\Component\HttpKernel\Bundle\Bundle::build().
    */
   public function build(ContainerBuilder $container) {
-    // Event listener.
-    $container->register('kernel.response', 'AppNeta\TraceView\TraceViewBundle\EventListener\KernelEventListener')
-      ->addTag('event_listener');
+    // Add Twig template support to the container.
+    parent::build($container);
+    $container->addCompilerPass(new RegisterTraceViewTwigPass());
   }
 }
 
