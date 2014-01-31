72ddc5ef391b9cb8667e67fa18b825a7224c06ee
James Meickle <jmeickle@appneta.com>
2014-01-31 17:05:47 -0500

    Rework tracing logic, remove deprecated Drupal code, and report C/A to match the Symfony debug toolbar.

--- EventDispatcher/TraceViewContainerAwareEventDispatcher.php
+++ EventDispatcher/TraceViewContainerAwareEventDispatcher.php
@@ -17,54 +17,64 @@ class TraceViewContainerAwareEventDispatcher extends ContainerAwareEventDispatch
 {
     public function dispatch($eventName, Event $event = null)
     {
+        // Store whether this event had any listeners when dispatch started. This
+        // prevents broken traces as a result of adding/removing listeners.
+        $had_listeners = $this->hasListeners($eventName);
 
         // Check whether this is a kernel request, response, or terminate.
         $is_request = ($eventName === "kernel.request");
         $is_response = ($eventName === "kernel.response");
         $is_terminate = ($eventName === "kernel.terminate");
 
-        // On the start of a kernel request or terminate, enter a layer.
-        if ($is_request) {
-            oboe_log(($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST) ? 'HttpKernel.master_request' : 'HttpKernel.sub_request', "entry", array(), TRUE);
-        } elseif ($is_terminate) {
-            oboe_log("HttpKernel.terminate", "entry", array(), TRUE);
-        }
-        // Enter a profile if the event has any listeners.
-        elseif ($this->hasListeners($eventName)) {
-            oboe_log("profile_entry", array('ProfileName' => $eventName), TRUE);
+        // If this event is being listened to, report a layer or profile entry.
+        if ($had_listeners) {
+            // On the start of a kernel request or terminate, enter a layer.
+            if ($is_request) {
+                oboe_log(($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST) ? 'HttpKernel.master_request' : 'HttpKernel.sub_request', "entry", array(), TRUE);
+            } elseif ($is_terminate) {
+                oboe_log("HttpKernel.terminate", "entry", array(), TRUE);
+            }
+            // Otherwise, enter a profile.
+            else {
+                oboe_log("profile_entry", array('ProfileName' => $eventName), TRUE);
+            }
         }
 
         // Dispatch the event as normal.
         $ret = parent::dispatch($eventName, $event);
 
-        // Capture controller/action information.
+        // If the event is a kernel controller, report controller/action information.
         if ($eventName === "kernel.controller") {
             $event_controller = $ret->getController();
 
-            // Handle the closure case, as per LegacyControllerSubscriber.
-            if (is_callable($event_controller) && is_object($event_controller)) {
-                $router_item = $event->getRequest()->attributes->get('drupal_menu_item');
-                $controller = $router_item['page_callback'];
-                $action = (isset($router_item['page_arguments'][0])) ? $router_item['page_arguments'][0] : NULL;
-            // Default to the object-based case.
-            } else {
+            // We use the same logic as the Symfony debug toolbar to parse out controller/action.
+            if (is_array($event_controller)) {
                 $controller = $event_controller[0];
                 $action = $event_controller[1];
+            } elseif ($controller instanceof \Closure) {
+                $r = new \ReflectionFunction($event_controller);
+                $controller = $r->getName();
+                $action = NULL;
+            } else {
+                $controller = (string) $event_controller ?: NULL;
+                $action = NULL;
             }
+
             // Report the C/A pair.
             oboe_log('info', array("Controller" => (is_object($controller)) ? get_class($controller) : $controller, "Action" => (is_object($action)) ? get_class($action) : $action));
         }
 
-        // Exit the profile if the event has any listeners.
-        if ($this->hasListeners($eventName)) {
-            oboe_log("profile_exit", array('ProfileName' => $eventName));
-        }
-
-        // On the end of a kernel response or terminate, exit the layer.
-        if ($is_response) {
-            oboe_log(($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST) ? 'HttpKernel.master_request' : 'HttpKernel.sub_request', "exit", array());
-        } elseif ($is_terminate) {
-            oboe_log('HttpKernel.terminate', "exit", array());
+        // If this event was being listened to, report a layer or profile exit.
+        if ($had_listeners) {
+            // On the end of a kernel response or terminate, exit a layer.
+            if ($is_response) {
+                oboe_log(($event->getRequestType() === HttpKernelInterface::MASTER_REQUEST) ? 'HttpKernel.master_request' : 'HttpKernel.sub_request', "exit", array());
+            } elseif ($is_terminate) {
+                oboe_log('HttpKernel.terminate', "exit", array());
+            // Otherwise, exit a profile.
+            } else {
+                oboe_log("profile_exit", array('ProfileName' => $eventName));
+            }
         }
 
         return $ret;
